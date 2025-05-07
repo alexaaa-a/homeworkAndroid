aa393f07ce5c2f5b21222e301cd2bb5057b323cb
alexaaa-a <144144005+alexaaa-a@users.noreply.github.com>
2025-05-07 22:16:32 +0300

    Delete app/src directory

--- app/src/main/java/com/first/homework/viewmodel/MainViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.first.homework.viewmodel
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-    fun validateCityName(city: String): Boolean {
-        return city.length in 3..50
-    }
-}
